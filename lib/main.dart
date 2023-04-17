b2456d77151b912836d42b09820badbe5bfdc161
Embler <vbembler@gmail.com>
2023-04-17 18:52:04 +0300

    created app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:open_weather/weather_home_page.dart';
+import 'package:open_weather/presentation/pages/weather_home_page.dart';
 
 void main() {
   runApp(const MyApp());
